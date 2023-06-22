f9484f98796a4d31f5d75b710dccff99b7a3d0d0
soso062 <129910015+soso062@users.noreply.github.com>
2023-06-21 21:54:14 -0300

    trava trava
    projeto trava tania butijão de gás

---
+++ javascript/exercicio1.js
@@ -0,0 +1,69 @@
+/*
+let pagamento = "debito"
+if (pagamento === "debito"){
+    console.log("recebe 10% de desconto");
+}else if (pagamento === "dinheiro" || pagamento === "pix"){
+    console.log("15% de desconto");
+}else if(pagamento === "2x"){
+    console.log("preço normal de etiqueta sem juros");
+}else{
+    console.log("peço normal de etiqueta mais juros de 10%")
+}
+*/
+/*
+switch (pagamento) {
+    case "debito":
+    console.log("recebe 10% de desconto");
+    break;    
+    case "dinheiro":
+    console.log("recebe 15% de desconto");
+    break;
+    case "pix":
+    console.log("recebe 15% de desconto");
+    break;
+    case "2x":
+    console.log("preço normal de etiqueta sem juros");
+    break;
+default:
+    console.log("preço normal de etiqueta mais juros de 10%");
+    break;
+}
+*/
+//let carro = "renegade";
+//switch (carro){
+  //  case "renegade":
+    //console.log("jeep");
+    //break;
+    //case "C4 Cactus":
+    //console.log("Citroen");
+    //break;
+    //case "HB20":
+    //console.log("Hyundai");
+    //break;
+    //case "Kicks":
+    //console.log("nissan");
+    //break;
+    //case "Focus":
+    //console.log("ford");
+    //break;
+    //case "Tracker":
+    //console.log("chevrolet");
+   // break;
+    //case "Corola":
+   // console.log("Toyota");
+   // break;
+//default:
+    //console.log("escolha alguma marca");
+    //break;
+//}
+
+for (let i = 0; i <= 10; i+2) {
+    console.log("o resultado é 5 x é",i, "=", 5 * i);
+    
+}
+
+
+
+
+
+
